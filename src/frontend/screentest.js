baf859c14aed0e7826288e38100bd4f9b77814f4
Sarin JANYAM <biglnwza18@gmail.com>
2026-01-31 21:17:11 +0700

    update screentest.js

--- src/frontend/screentest.js
+++ src/frontend/screentest.js
@@ -2,5 +2,5 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function screentest() {
-  <Text>Test1232 </Text>
+  <Text>HelloHippo </Text>
 }
